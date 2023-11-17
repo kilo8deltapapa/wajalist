a7409fb150c71b2951737d79025d3f637bca1205
kilo8deltapapa <150816938+kilo8deltapapa@users.noreply.github.com>
2023-11-17 13:40:16 -0500

    Add files via upload
    Pythonic improvements

--- wajalist.py
+++ wajalist.py
@@ -4,8 +4,8 @@
 
 """
 Description: This program generates a list of Japan Prefectures that have been
-confirmed, a list of unconfirmed 
-prefectures, and a mapchartSave.txt file for upload to 
+confirmed, a list of unconfirmed
+prefectures, and a mapchartSave.txt file for upload to
 https://www.mapchart.net/japan.html to visualize the prefectures.
 
 The following ADIF tags in the file being analyzied are used:
@@ -17,7 +17,7 @@ The following ADIF tags in the file being analyzied are used:
 <SAT_NAME> Name of satellite used; Optional, needed for -s option
 <PROP_MODE> Propagation mode; Optional, needed for --satonly and --nosat options
 
-This can be obtained by using the ARRL Logbook of the World Query by 
+This can be obtained by using the ARRL Logbook of the World Query by
 Rick Murphy K1MU found at URL: https://www.rickmurphy.net/lotwquery.htm
 
 Author: Douglas C. Papay K8DP
@@ -30,59 +30,63 @@ License: MIT License
 """
 
 import sys
-import re
 import argparse
 import adif_io
 
 # Initialize parser
 parser = argparse.ArgumentParser()
- 
+
 # Adding optional argument
 parser.add_argument('filename')
-parser.add_argument("-c", "--call", nargs='+', help = "list of callsigns, if not specified all callsigns are considered")
-parser.add_argument("-b", "--band", help = "band (eg. 20M, 2M, 70CM, etc...), if not specified all bands are considered")
-parser.add_argument("-m", "--mode", help = "mode (eg. CW, SSB, FT8, etc...), if not specified all modes are considered")
-parser.add_argument("-s", "--sat", help = "satellite name (eg. RS-44, IO-117, etc...), if not specified all satellites are included")
-parser.add_argument("--satonly", action='store_true', help = "include only satellite QSOs")
-parser.add_argument("--nosat", action='store_true', help = "exclude satellite QSOs")
+parser.add_argument("-c", "--call", nargs='+', help = "list of callsigns, if \
+    not specified all callsigns are considered")
+parser.add_argument("-b", "--band", help = "band (eg. 20M, 2M, 70CM, \
+    etc...), if not specified all bands are considered")
+parser.add_argument("-m", "--mode", help = "mode (eg. CW, SSB, FT8, etc...), \
+    if not specified all modes are considered")
+parser.add_argument("-s", "--sat", help = "satellite name (eg. RS-44, IO-117, \
+    etc...), if not specified all satellites are included")
+parser.add_argument("--satonly", action='store_true', help = "include only \
+    satellite QSOs")
+parser.add_argument("--nosat", action='store_true', help = "exclude satellite \
+    QSOs")
 
 # Read arguments from command line
 args = parser.parse_args()
 if args.filename:
     input_filename = args.filename
 else:
-    quit()
-    
+    sys.exit()
+
 if args.call:
     callsign_list = args.call
     callsign_list = [x.upper() for x in callsign_list]
-else:    
+else:
     callsign_list = []
 
 if args.band:
-    band = args.band.upper()
-else:    
-    band = ""
-    
+    BAND = args.band.upper()
+else:
+    BAND = ""
+
 if args.mode:
-    mode = args.mode.upper()
-else:    
-    mode = ""    
-    
+    MODE = args.mode.upper()
+else:
+    MODE = ""
+
 if args.sat:
-    sat = args.sat.upper()
-else:    
-    sat = ""    
-    
+    SAT = args.sat.upper()
+else:
+    SAT = ""
+
 #Read prefecture list from file
 pref_defs = []
-with open("ja_prefectures.txt", "r") as file2:
+with open("ja_prefectures.txt", "r", encoding="utf-8") as file2:
     while True:
         line = file2.readline().strip()
         if not line:
             break
-        else:
-            pref_defs.append(line.split(" "))
+        pref_defs.append(line.split(" "))
 
 prefs_list = []
 needed_list = []
@@ -102,30 +106,34 @@ for qso in qsos_raw:
 
     #not all records have sat_name
     try:
-        sat_name = qso["SAT_NAME"].upper()
-    except:
-        sat_name = ""
+        SAT_NAME = qso["SAT_NAME"].upper()
+    except KeyError as e:
+        #key does not exist
+        SAT_NAME = ""
     try:
-        prop_mode = qso["PROP_MODE"]
-    except:
-        prop_mode = ""
-    
+        PROP_MODE = qso["PROP_MODE"]
+    except KeyError as e:
+        #key does not exist
+        PROP_MODE = ""
+
     try:
-        # read only records that are with USA DXCC = 291     
+        # read only records that are with Japan DXCC = 339
         if qso["DXCC"] == '339' \
-        and ((station_callsign in callsign_list) or not len(callsign_list)) \
-        and (sat_name == sat or sat == "") \
-        and (qso["MODE"] == mode or mode == "") \
-        and ((prop_mode == "SAT" and args.satonly) or (not args.satonly and not args.nosat) or (prop_mode == "" and args.nosat)) \
-        and (qso["BAND"] == band or band == ""):
+        and ((station_callsign in callsign_list) or len(callsign_list) == 0) \
+        and (SAT in (SAT_NAME, '') or SAT == "") \
+        and (MODE in (qso['MODE'], '') or MODE == "") \
+        and ((PROP_MODE == "SAT" and args.satonly) or (not args.satonly \
+            and not args.nosat) or (PROP_MODE == "" and args.nosat)) \
+        and (BAND in (qso['BAND'], '') or BAND == ""):
             if qso["STATE"] not in prefs_list:
-                prefs_list.append(qso["STATE"])                
-    except:
+                prefs_list.append(qso["STATE"])
+    except KeyError as e:
+        #other key does not exist
         pass
-        
+
 print("Callsigns in ADIF = ", ', '.join(qsocall_list),"\n")
 print(len(qsos_raw), "records in ADIF\n")
-        
+
 #sort the prefecture list
 prefs_list.sort()
 
@@ -147,30 +155,37 @@ for p in needed_list:
 
 print("\n")
 
-if(len(prefs_list) > 0):
+if len(prefs_list) > 0:
     print("Generating mapchart.net file...")
 
-    with open("mapchartSave-ja.txt", "w") as f:
+    with open("mapchartSave-ja.txt", "w", encoding="utf-8") as f:
         print('{"groups":{"#e0ecf4":{"label":"Confirmed LoTW","paths":[', end="", file=f)
 
         for i,p in enumerate(prefs_list):
             if i < len(prefs_list) - 1:
-               print('"' + pref_defs[int(p)-1][1] + '"',end=",", file=f)
+                print('"' + pref_defs[int(p)-1][1] + '"',end=",", file=f)
             else:
-               print('"' + pref_defs[int(p)-1][1] + '"',end="", file=f)
+                print('"' + pref_defs[int(p)-1][1] + '"',end="", file=f)
 
         print(']},"#cc3333":{"label":"Needed","paths":[',end="", file=f)
 
         for i,p in enumerate(needed_list):
             if i < len(needed_list) - 1:
-               print('"' + pref_defs[int(p)-1][1] + '"',end=",", file=f)
+                print('"' + pref_defs[int(p)-1][1] + '"',end=",", file=f)
             else:
-               print('"' + pref_defs[int(p)-1][1] + '"', file=f)
+                print('"' + pref_defs[int(p)-1][1] + '"', file=f)
 
-        print(']}},"title":"'+ ', '.join(callsign_list) +'Worked All Japan Prefectures","hidden":[],"background":"#fff","borders":"#000","legendFont":"Helvetica","legendFontColor":"#000","legendBgColor":"#00000000","legendWidth":150,"areBordersShown":true,"defaultColor":"#d1dbdd","labelsColor":"#6a0707","strokeWidth":"medium","areLabelsShown":true,"legendPosition":"top_left","legendSize":"medium","legendStatus":"show","scalingPatterns":true,"legendRowsSameColor":true,"legendColumnCount":2}', file=f)
+        print(']}},"title":"' + ', '.join(callsign_list) + 'Worked All Japan \
+        Prefectures","hidden":[],"background":"#fff","borders":"#000",\
+        "legendFont":"Helvetica","legendFontColor":"#000",\
+        "legendBgColor":"#00000000","legendWidth":150,"areBordersShown":true,\
+        "defaultColor":"#d1dbdd","labelsColor":"#6a0707",\
+        "strokeWidth":"medium","areLabelsShown":true,\
+        "legendPosition":"top_left","legendSize":"medium",\
+        "legendStatus":"show","scalingPatterns":true,\
+        "legendRowsSameColor":true,"legendColumnCount":2}', file=f)
 
     print("   Done.\n")
     print("File can be uploaded to https://www.mapchart.net/japan.html for map display.\n")
 else:
     print("No prefectures confirmed, mapchart.net file was not created!")
-    
